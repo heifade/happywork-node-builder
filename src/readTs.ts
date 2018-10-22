ce1572332a067e7e95049c337caafa49c239aef8
heifade <heifade@126.com>
2018-10-22 08:57:23 +0800

    1.0.29

--- src/readTs.ts
+++ src/readTs.ts
@@ -3,15 +3,17 @@ import { unlinkSync, existsSync, mkdirSync, rmdirSync } from "fs";
 import { spawn } from "child_process";
 
 function getTsc() {
-  const path1 = resolvePath(__dirname, "../node_modules/typescript/bin/tsc"); // 打包后 __dirname 变成 dist， 所以是'..'
-  if (existsSync(path1)) {
-    return path1;
-  }
-  const path2 = resolvePath(process.cwd(), "./node_modules/typescript/bin/tsc");
-  if (existsSync(path2)) {
-    return path2;
-  }
-  throw new Error("tsc is not exists!");
+  // const path1 = resolvePath(__dirname, "../node_modules/typescript/bin/tsc"); // 打包后 __dirname 变成 dist， 所以是'..'
+  // if (existsSync(path1)) {
+  //   return path1;
+  // }
+  // const path2 = resolvePath(process.cwd(), "./node_modules/typescript/bin/tsc");
+  // if (existsSync(path2)) {
+  //   return path2;
+  // }
+  // throw new Error("tsc is not exists!");
+
+  return 'tsc';
 }
 
 export async function readTs(file: string) {
