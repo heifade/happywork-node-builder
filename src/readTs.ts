65c14257cf76dc518e96710c2fd4d5b15b7e878c
heifade <heifade@126.com>
2018-10-27 21:22:14 +0800

    no message

--- src/readTs.ts
+++ src/readTs.ts
@@ -1,5 +1,6 @@
-import { unlinkSync, existsSync } from "fs";
+import { unlinkSync, existsSync, readFileSync } from "fs";
 import { spawnSync } from "child_process";
+import { transpileModule, ModuleKind, ScriptTarget } from "typescript";
 
 export async function readTs(file: string) {
   return new Promise<any>((resolve, reject) => {
@@ -25,4 +26,18 @@ export async function readTs(file: string) {
       unlinkSync(jsFileName);
     }
   });
+}
+
+function compile(tsFile: string) {
+  const code = readFileSync(tsFile, { encoding: "utf8" });
+  const transformed = transpileModule(code, {
+    compilerOptions: {
+      noEmitHelpers: false,
+      isolatedModules: false,
+      module: ModuleKind.CommonJS,
+      target: ScriptTarget.ES2015,
+      noImplicitUseStrict: true
+    }
+  });
+  return transformed.outputText;
 }
